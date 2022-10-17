8fb64e15df013f78d994158fda0865830a3b51de
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2022-10-17 16:58:55 +0530

    fix media item paths in example

--- example/example.ts
+++ example/example.ts
@@ -2,7 +2,7 @@ import { Player } from '../src/player';
 
 document.addEventListener('click', () => {
   const player = new Player();
-  player.addMediaItem('/static/1.mp3');
-  player.addMediaItem('/static/2.mp3');
+  player.addMediaItem('static/1.mp3');
+  player.addMediaItem('static/2.mp3');
   player.play();
 });
