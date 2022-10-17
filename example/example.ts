35a640d320b850f9c26b73d362c468b99b57bce5
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2022-10-17 19:43:55 +0530

    add button to the example because why not

--- example/example.ts
+++ example/example.ts
@@ -1,8 +1,20 @@
 import { Player } from '../src/player';
 
-document.addEventListener('click', () => {
+function main() {
   const player = new Player();
   player.addMediaItem('static/1.mp3');
   player.addMediaItem('static/2.mp3');
-  player.play();
-});
+  document
+    .querySelector('#play')
+    ?.addEventListener('click', () => player.play());
+
+  document
+    .querySelector('#pause')
+    ?.addEventListener('click', () => player.pause());
+
+  document
+    .querySelector('#stop')
+    ?.addEventListener('click', () => player.stop());
+}
+
+document.addEventListener('DOMContentLoaded', () => main());
