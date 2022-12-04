ad073a5ab02c51a53c474d291b0202e06fce994d
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2022-12-04 20:26:40 +0530

    demonstrate gapless playback using web audio

--- src/player.ts
+++ src/player.ts
@@ -1,161 +1,123 @@
+import MediaItem from './media-item';
+
+const AudioContextImpl: typeof AudioContext = window.AudioContext;
+
 export class Player {
-  private static readonly CROSS_FADE_BUFFER = 0.5;
-  private static readonly CROSS_FADE_DURATION = 1.5;
+  private readonly context: AudioContext = new AudioContextImpl();
+  private readonly gainNode = this.context.createGain();
 
-  private playlist: string[] = [];
-  private audioQueue: HTMLAudioElement[] = [];
+  private playlist: MediaItem[] = [];
+  private buffering = false;
+  private nextChunkStartTime = this.context.currentTime;
   private playWhenReady = false;
-  private volume = 1.0;
-  private isCrossFading = false;
+  private bufferTicker?: number;
+  private bufferSizeSeconds: number;
 
-  public play() {
+  constructor(bufferSizeSeconds: number) {
+    this.bufferSizeSeconds = bufferSizeSeconds;
+    this.gainNode.gain.value = 1.0;
+    this.gainNode.connect(this.context.destination);
+  }
+
+  public async play() {
+    if (this.context.state === 'closed') {
+      throw new Error('attempted to restarted stopped player');
+    }
+
     this.playWhenReady = true;
-    if (this.audioQueue.length > 0) {
-      this.playAudio(this.audioQueue[0]);
-    } else if (this.audioQueue.length < 1) {
-      this.queueNextItem();
+    if (!this.buffering) {
+      this.scheduleBufferTicker();
+    }
+
+    if (this.context.state === 'suspended') {
+      await this.context.resume();
     }
   }
 
-  public pause() {
+  public async pause() {
     this.playWhenReady = false;
-    if (this.audioQueue.length < 1) {
+    if (this.context.state === 'running') {
+      await this.context.suspend();
+    }
+  }
+
+  public async stop() {
+    clearTimeout(this.bufferTicker);
+    await this.pause();
+    if (this.context.state !== 'closed') {
+      await this.context.close();
+    }
+  }
+
+  public setVolume(volume: number): void {
+    this.gainNode.gain.value = volume;
+  }
+
+  public addMediaItem(src: string): void {
+    this.playlist.push(new MediaItem(src));
+    if (this.playWhenReady && !this.buffering) {
+      this.scheduleBufferTicker();
+    }
+  }
+
+  private async buffer() {
+    this.buffering = true;
+    if (this.playlist.length < 1) {
+      this.buffering = false;
+      console.log('all media items in the playlist have finished buffering');
       return;
     }
 
-    this.audioQueue[0].pause();
-    if (this.isCrossFading) {
-      // if the player is cross fading, audio queue must have at least two
-      // elements. Skip cross fade transition, discard the ending item and reset
-      // the next item for normal playback.
-      this.isCrossFading = false;
-      this.audioQueue.shift();
-      this.audioQueue[0].pause(); // pause cross fading item.
-      this.audioQueue[0].volume = this.volume; // restore volume
-    }
-  }
-
-  public stop() {
-    this.pause();
-    this.audioQueue = [];
-  }
-
-  public setVolume(volume: number) {
-    this.volume = volume;
-    if (this.isCrossFading) {
+    const remaining = this.nextChunkStartTime - this.context.currentTime;
+    if (remaining > this.bufferSizeSeconds) {
+      console.log('buffered duration exceeds the requested buffer size');
+      this.scheduleBufferTicker();
       return;
     }
 
-    this.audioQueue.forEach((a) => (a.volume = volume));
-  }
-
-  public addMediaItem(src: string) {
-    this.playlist.push(src);
-    if (this.audioQueue.length < 1) {
-      this.queueNextItem();
+    if (!this.playlist[0].isInitialized()) {
+      console.log('init media item');
+      await this.playlist[0].initialize();
     }
-  }
 
-  private queueNextItem() {
-    const url = this.playlist.shift();
-    if (url == null) {
-      console.log('playlist is empty');
+    if (!this.playlist[0].hasNextChunk()) {
+      console.log('finished media item');
+      this.playlist.shift();
+      this.scheduleBufferTicker();
       return;
     }
 
-    console.log('queuing', url);
-    const audio = new Audio(url);
-    audio.volume = this.volume;
-    audio.preload = 'auto';
-    audio.addEventListener('ended', () => {
-      this.audioQueue.shift();
-      if (this.audioQueue[0]?.paused) {
-        this.audioQueue[0].volume = this.volume;
-        this.playAudio(this.audioQueue[0]);
-      }
-    });
+    const chunk = await this.playlist[0].getNextChunk();
+    await this.appendToAudioContext(chunk);
+    console.log('appended to audio context');
 
-    const itemTransitioner = () => {
-      const remaining = audio.duration - audio.currentTime;
-      if (remaining > Player.CROSS_FADE_DURATION + Player.CROSS_FADE_BUFFER) {
-        return;
-      }
+    if (this.playWhenReady && this.context.state === 'suspended') {
+      this.context.resume();
+    }
 
-      if (this.audioQueue.length > 1) {
-        this.crossFadeNextItem();
-        audio.removeEventListener('timeupdate', itemTransitioner);
-      }
+    this.scheduleBufferTicker();
+  }
+
+  private scheduleBufferTicker() {
+    this.bufferTicker = setTimeout(() => this.buffer(), 500);
+  }
+
+  private async appendToAudioContext(chunk: ArrayBuffer) {
+    const buffer = await this.context.decodeAudioData(chunk);
+    const source = this.context.createBufferSource();
+    source.buffer = buffer;
+    source.onended = () => {
+      source.disconnect();
+      console.log('finished chunk');
     };
 
-    audio.addEventListener('timeupdate', itemTransitioner);
-    const nextItemQueuer = () => {
-      const remaining = audio.duration - audio.currentTime;
-      if (remaining < 15 && this.audioQueue.length < 2) {
-        this.queueNextItem();
-        audio.removeEventListener('timeupdate', nextItemQueuer);
-      }
-    };
-
-    audio.addEventListener('timeupdate', nextItemQueuer);
-    this.audioQueue.push(audio);
-    if (this.playWhenReady && this.audioQueue.length === 1) {
-      this.playAudio(audio);
-    }
-  }
-
-  private async crossFadeNextItem() {
-    this.isCrossFading = true;
-    const first = this.audioQueue[0];
-    const second = this.audioQueue[1];
-    first.volume = this.volume;
-    second.volume = 0;
-    try {
-      await second.play();
-    } catch (e) {
-      console.warn('failed to play audio', second.src, e);
-      first.pause();
-      this.audioQueue = this.audioQueue.slice(2);
-      this.queueNextItem();
-      return;
+    if (this.nextChunkStartTime < this.context.currentTime) {
+      this.nextChunkStartTime = this.context.currentTime;
     }
 
-    const crossFader = () => {
-      if (!this.isCrossFading) {
-        return;
-      }
-
-      const remaining =
-        first.duration - first.currentTime - Player.CROSS_FADE_BUFFER;
-      if (remaining <= 0) {
-        first.volume = 0;
-        second.volume = this.volume;
-        this.isCrossFading = false;
-      } else {
-        const fraction = remaining / Player.CROSS_FADE_DURATION;
-        first.volume = this.volume * Math.sqrt(fraction);
-        second.volume = this.volume * Math.sqrt(1 - fraction);
-        setTimeout(crossFader, 25);
-      }
-
-      console.log(
-        'cross fade volumes:',
-        'out:',
-        first.volume,
-        'in:',
-        second.volume
-      );
-    };
-
-    setTimeout(crossFader, 0);
-  }
-
-  private async playAudio(audio: HTMLAudioElement) {
-    try {
-      return await audio.play();
-    } catch (e) {
-      console.warn('failed to play audio', audio.src, e);
-      this.audioQueue.shift();
-      this.queueNextItem();
-    }
+    source.connect(this.gainNode);
+    source.start(this.nextChunkStartTime);
+    this.nextChunkStartTime += buffer.duration;
+    console.log(buffer.duration);
   }
 }
